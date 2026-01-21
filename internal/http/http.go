d28bc1aaff186b6f7ca38113f8e294927cb01788
Fergus Molloy <fergus@molloy.xyz>
2026-01-21 09:34:15 +0000

    add http server functions

---
+++ internal/http/http.go
@@ -0,0 +1,45 @@
+package http
+
+import (
+	"context"
+	"log/slog"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
+	"time"
+)
+
+// StartHttpServer creates and starts a [http.Server]. Returns the server so that shutdown can be called.
+func StartHttpServer(wg *sync.WaitGroup, addr string) *http.Server {
+	srv := &http.Server{
+		Addr:         addr,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go serveHttpServer(wg, srv)
+
+	return srv
+}
+
+func serveHttpServer(wg *sync.WaitGroup, srv *http.Server) {
+	defer wg.Done()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		slog.Error("error while serving http server", "err", err, "addr", srv.Addr)
+	}
+}
+
+func StartPprofServer(wg *sync.WaitGroup, addr string) *http.Server {
+	srv := &http.Server{
+		Addr:         addr,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      http.DefaultServeMux,
+	}
+
+	go serveHttpServer(wg, srv)
+
+	return srv
+}
