432454361cf475aedc3f0e735cbc8cb0721fc62d
Fergus Molloy <fergus@molloy.xyz>
2026-01-30 10:45:44 +0000

    move queue to it's own package

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -2,15 +2,21 @@ package broker
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"sync"
 
 	"fergus.molloy.xyz/vfmp/internal/model"
+	"fergus.molloy.xyz/vfmp/internal/queue"
+)
+
+var (
+	ErrNotFound = errors.New("queue not found")
 )
 
 type Broker struct {
 	mu      *sync.Mutex
-	topics  map[string]*Queue
+	topics  map[string]*queue.Queue
 	MsgChan chan model.Message
 }
 
@@ -29,14 +35,14 @@ func (b *Broker) awaitMessages(ctx context.Context, wg *sync.WaitGroup) {
 	}
 }
 
-func (b *Broker) getOrCreateTopic(ctx context.Context, topic string) *Queue {
+func (b *Broker) getOrCreateTopic(ctx context.Context, topic string) *queue.Queue {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
 	q, ok := b.topics[topic]
 	if !ok {
 		slog.Info("creating new queue", "topic", topic)
-		q = NewQueue(ctx)
+		q = queue.New(ctx, topic)
 		b.topics[topic] = q
 	}
 	return q
@@ -45,7 +51,7 @@ func (b *Broker) getOrCreateTopic(ctx context.Context, topic string) *Queue {
 func StartBroker(ctx context.Context, wg *sync.WaitGroup) *Broker {
 	b := &Broker{
 		mu:      new(sync.Mutex),
-		topics:  make(map[string]*Queue),
+		topics:  make(map[string]*queue.Queue),
 		MsgChan: make(chan model.Message, 1),
 	}
 
@@ -54,7 +60,8 @@ func StartBroker(ctx context.Context, wg *sync.WaitGroup) *Broker {
 	return b
 }
 
-func (b *Broker) NotifyReady(ctx context.Context, topic string) chan model.Message {
+func (b *Broker) NotifyReady(ctx context.Context, topic string) <-chan model.Message {
 	q := b.getOrCreateTopic(ctx, topic)
-	return q.MsgChan
+	return q.GetMsgChan()
+}
 }
---
+++ internal/queue/queue_test.go
@@ -0,0 +1,70 @@
+package queue
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"fergus.molloy.xyz/vfmp/internal/model"
+)
+
+// TestAppendDoesntBlock asserts that we can always append to the queue, even
+// when no messages are being consumed
+func TestAppendDoesntBlock(t *testing.T) {
+	ctx := t.Context()
+	q := New(ctx, "topic")
+
+	q.Append(model.Message{Data: []byte("data"), Topic: "topic"})
+
+	timeoutCtx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+	go func() {
+		for range 10 {
+			q.Append(model.Message{Data: []byte("data 2"), Topic: "topic"})
+			time.Sleep(time.Millisecond * 10)
+		}
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-done:
+		return
+	case <-timeoutCtx.Done():
+		t.Fatalf("timedout waiting to append new message to queue")
+	}
+}
+
+func TestLengthIncrementsCorrectly(t *testing.T) {
+	ctx := t.Context()
+	q := New(ctx, "topic")
+
+	if q.Len() != 0 {
+		t.Fatalf("queue has incorrect length, got: %d, want: %d", q.Len(), 0)
+	}
+
+	q.Append(model.Message{})
+	time.Sleep(10 * time.Millisecond)
+	if q.Len() != 1 {
+		t.Fatalf("queue has incorrect length, got: %d, want: %d", q.Len(), 1)
+	}
+
+	q.Append(model.Message{})
+	time.Sleep(10 * time.Millisecond)
+	if q.Len() != 2 {
+		t.Fatalf("queue has incorrect length, got: %d, want: %d", q.Len(), 2)
+	}
+
+	<-q.GetMsgChan()
+	time.Sleep(10 * time.Millisecond)
+	if q.Len() != 1 {
+		t.Fatalf("queue has incorrect length, got: %d, want: %d", q.Len(), 1)
+	}
+
+	<-q.GetMsgChan()
+	time.Sleep(10 * time.Millisecond)
+	if q.Len() != 0 {
+		t.Fatalf("queue has incorrect length, got: %d, want: %d", q.Len(), 0)
+	}
+}
